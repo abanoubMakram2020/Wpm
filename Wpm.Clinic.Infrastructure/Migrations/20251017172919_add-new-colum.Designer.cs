31234180ffb3001982c0317149604b01aa4d437c
Abanoub Makram <amakram@RASAN.CO>
2025-10-17 20:45:48 +0300

    commit changes

--- Wpm.Clinic.Infrastructure/Migrations/20251017172919_add-new-colum.Designer.cs
+++ Wpm.Clinic.Infrastructure/Migrations/20251017172919_add-new-colum.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Wpm.Clinic.Infrastructure;
 
@@ -11,9 +12,11 @@ using Wpm.Clinic.Infrastructure;
 namespace Wpm.Clinic.Infrastructure.Migrations
 {
     [DbContext(typeof(ClinicDBContext))]
-    partial class ClinicDBContextModelSnapshot : ModelSnapshot
+    [Migration("20251017172919_add-new-colum")]
+    partial class addnewcolum
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
